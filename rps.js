90be6134eee27255f0ff7dec9cf3a0295e6331d6
HL <han.pecktree@gmail.com>
2023-02-25 17:50:11 -0700

    Add main game function

--- rps.js
+++ rps.js
@@ -4,6 +4,7 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    // 
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
     const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
@@ -17,9 +18,33 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
+    // main game function which plays 5 rounds keeping track of score and console logging the result
     
+    // keep track of player and computer scores
+    let playerScore = 0;
+    let computerScore = 0;
+
+    // loop through 5 rounds and console log each round
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Input rock, paper, or scissors");
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        if (result.includes("Win")) {
+            ++playerScore;
+        } else if (result.includes("Lose")) {
+            ++computerScore;
+        }
+        console.log(result);
+    }
+
+    // console log the final score and result
+    if (playerScore > computerScore) {
+        console.log(`You won the game. You won ${playerScore} rounds and lost ${computerScore} rounds`);
+    } else if (playerScore < computerScore) {
+        console.log(`You lost the game. You won ${playerScore} rounds and lost ${computerScore} rounds`);
+    } else {
+        console.log(`The game was a draw. You won ${playerScore} rounds and lost ${computerScore} rounds`);
+    }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+game();
