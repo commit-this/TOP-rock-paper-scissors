173addc9fe6af8eaf066598db146de1df47e4bc6
HL <han.pecktree@gmail.com>
2023-03-01 21:53:35 -0700

    Replace console log functions with eventlistener click

--- rps.js
+++ rps.js
@@ -4,50 +4,82 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(e) {
     // function that takes player and computer choices and returns a string detailing the result
-    // TODO: make sure the player inputs rock paper or scissors only
-    
-    // capitalize the player selection
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    const playerSelection = e.target.id;
+    const computerSelection = getComputerChoice();
+    const result = document.querySelector(".result");
+    const displayYourScore = document.querySelector(".playerScore");
+    const displayComputerScore = document.querySelector(".computerScore");
+
+    let playerScore = 0;
+    let computerScore = 0;
     const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
     const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
 
-    return playerSelection === "Rock" && computerSelection === "Paper" ? loseMessage
+    const resultText = playerSelection === "Rock" && computerSelection === "Paper" ? loseMessage
             : playerSelection === "Paper" && computerSelection === "Scissors" ? loseMessage
             : playerSelection === "Scissors" && computerSelection === "Rock" ? loseMessage
             : playerSelection === computerSelection ? "Draw!"
             : winMessage;
-}
 
-function game() {
-    // main game function which plays 5 rounds keeping track of score and console logging the result
     
-    // keep track of player and computer scores
-    let playerScore = 0;
-    let computerScore = 0;
-
-    // loop through 5 rounds and console log each round
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Input rock, paper, or scissors");
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        if (result.includes("Win")) {
-            playerScore++;
-        } else if (result.includes("Lose")) {
-            computerScore++;
-        }
-        console.log(result);
+    if (resultText == winMessage) {
+        playerScore += 1;
+    } else if (resultText == loseMessage) {
+        computerScore += 1;
     }
+    if (playerScore == 5) {
+        score.textContent = "Congratulations, you won 5 rounds";
 
-    // console log the final score and result
-    if (playerScore > computerScore) {
-        console.log(`You won the game. You won ${playerScore} rounds and lost ${computerScore} rounds`);
-    } else if (playerScore < computerScore) {
-        console.log(`You lost the game. You won ${playerScore} rounds and lost ${computerScore} rounds`);
-    } else {
-        console.log(`The game was a draw. You won ${playerScore} rounds and lost ${computerScore} rounds`);
+    } else if (computerScore == 5) {
+        score.textContent = "Sorry, you lost 5 rounds";
     }
+    result.textContent = `${resultText}`;
+    displayYourScore.textContent = `Your score: ${playerScore}`;
+    displayComputerScore.textContent = `Computer score: ${computerScore}`;
+
 }
 
-game();
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => button.addEventListener("click", playRound));
+
+
+/******************************************* 
+
+Legacy code from when this was console only
+
+********************************************/
+
+// function game() {
+//     // main game function which plays 5 rounds keeping track of score and console logging the result
+    
+//     // keep track of player and computer scores
+//     let playerScore = 0;
+//     let computerScore = 0;
+
+//     // loop through 5 rounds and console log each round
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection = prompt("Input rock, paper, or scissors");
+//         let computerSelection = getComputerChoice();
+//         let result = playRound(playerSelection, computerSelection);
+//         if (result.includes("Win")) {
+//             playerScore++;
+//         } else if (result.includes("Lose")) {
+//             computerScore++;
+//         }
+//         console.log(result);
+//     }
+
+//     // console log the final score and result
+//     if (playerScore > computerScore) {
+//         console.log(`You won the game. You won ${playerScore} rounds and lost ${computerScore} rounds`);
+//     } else if (playerScore < computerScore) {
+//         console.log(`You lost the game. You won ${playerScore} rounds and lost ${computerScore} rounds`);
+//     } else {
+//         console.log(`The game was a draw. You won ${playerScore} rounds and lost ${computerScore} rounds`);
+//     }
+// }
+
+// game();
