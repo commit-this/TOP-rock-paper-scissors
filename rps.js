86c10e29df16b0f3a33b813836d4f6a8d10221a7
Han Li <han.pecktree@gmail.com>
2023-03-03 21:22:18 -0700

    Add reset button and functionality

--- rps.js
+++ rps.js
@@ -5,14 +5,10 @@ function getComputerChoice() {
 }
 
 function playRound(e) {
-    // function that takes player and computer choices and returns a string detailing the result
+    // function that compares player and computer choices and displays the result as text on the html page
 
     const playerSelection = e.target.id;
     const computerSelection = getComputerChoice();
-    const result = document.querySelector(".result");
-    const displayYourScore = document.querySelector(".playerScore");
-    const displayComputerScore = document.querySelector(".computerScore");
-    const gameStatus = document.querySelector(".win");
 
     const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
     const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
@@ -23,64 +19,47 @@ function playRound(e) {
             : playerSelection === computerSelection ? "Draw!"
             : winMessage;
 
-    
-    if (resultText == winMessage) {
+    // prevent game from continuing after 5 rounds
+    if (gameStatus.textContent) return;
+
+    if (resultText === winMessage) {
         playerScore += 1;
-    } else if (resultText == loseMessage) {
+    } else if (resultText === loseMessage) {
         computerScore += 1;
     }
-    if (playerScore == 5) {
+    
+    if (playerScore === 5) {
         gameStatus.textContent = "Congratulations, you won 5 rounds";
 
-    } else if (computerScore == 5) {
+    } else if (computerScore === 5) {
         gameStatus.textContent = "Sorry, you lost 5 rounds";
     }
+    
     result.textContent = `${resultText}`;
     displayYourScore.textContent = `Your score: ${playerScore}`;
     displayComputerScore.textContent = `Computer score: ${computerScore}`;
-
 }
 
+function reset(e) {
+    // function that resets scores and clears text on the html page
+    playerScore = 0;
+    computerScore = 0;
+    result.textContent = "";
+    displayComputerScore.textContent = "";
+    displayYourScore.textContent = "";
+    gameStatus.textContent = "";
+}
+
+const result = document.querySelector(".result");
+const displayYourScore = document.querySelector(".playerScore");
+const displayComputerScore = document.querySelector(".computerScore");
+const gameStatus = document.querySelector(".win");
+const resetButton = document.querySelector(".reset");
+const buttons = document.querySelectorAll(".btn");
 let playerScore = 0;
 let computerScore = 0;
-const buttons = document.querySelectorAll(".btn");
+
 buttons.forEach(button => button.addEventListener("click", playRound));
+resetButton.addEventListener("click", reset);
 
 
-/******************************************* 
-
-Legacy code from when this was console only
-
-********************************************/
-
-// function game() {
-//     // main game function which plays 5 rounds keeping track of score and console logging the result
-    
-//     // keep track of player and computer scores
-//     let playerScore = 0;
-//     let computerScore = 0;
-
-//     // loop through 5 rounds and console log each round
-//     for (let i = 0; i < 5; i++) {
-//         let playerSelection = prompt("Input rock, paper, or scissors");
-//         let computerSelection = getComputerChoice();
-//         let result = playRound(playerSelection, computerSelection);
-//         if (result.includes("Win")) {
-//             playerScore++;
-//         } else if (result.includes("Lose")) {
-//             computerScore++;
-//         }
-//         console.log(result);
-//     }
-
-//     // console log the final score and result
-//     if (playerScore > computerScore) {
-//         console.log(`You won the game. You won ${playerScore} rounds and lost ${computerScore} rounds`);
-//     } else if (playerScore < computerScore) {
-//         console.log(`You lost the game. You won ${playerScore} rounds and lost ${computerScore} rounds`);
-//     } else {
-//         console.log(`The game was a draw. You won ${playerScore} rounds and lost ${computerScore} rounds`);
-//     }
-// }
-
-// game();
